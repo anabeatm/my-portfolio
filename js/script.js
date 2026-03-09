5ab694043937c1036da1e48db790d13c8bbc1740
Ana Beatriz Tavares Malaquais <ana.beat20051@gmail.com>
2026-03-09 12:21:32 -0300

    feat: stylle social media and footer in CSS

--- js/script.js
+++ js/script.js
@@ -72,6 +72,98 @@ async function apiGithub() {
   }
 }
 
+// function openWindow(windowID) {
+//   const windowElement = document.getElementById(windowID);
+//   if (windowElement) {
+//     windowElement.style.display = "flex";
+//     bringToFront(windowElement);
+//   }
+// }
+
+// function closeWindow(windowID) {
+//   const windowElement = document.getElementById(windowID);
+//   if (windowElement) {
+//     windowElement.style.display = "none";
+//   }
+// }
+
+// let zIndexCounter = 100;
+
+// function bringToFront(element) {
+//   zIndexCounter++;
+//   element.style.zIndex = zIndexCounter;
+// }
+
+// document.getElementById("btn-projects").addEventListener("click", (e) => {
+//   e.preventDefault();
+//   openWindow("window-projects");
+// });
+
+// document.getElementById("btn-contact").addEventListener("click", (e) => {
+//   e.preventDefault();
+//   openWindow("window-contact");
+// });
+
+// document.querySelectorAll(".close-btn").forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     const targetID = btn.getAttribute("data-target");
+//     closeWindow(targetID);
+//   });
+// });
+
+// document.querySelectorAll(".window").forEach((win) => {
+//   win.addEventListener("mousedown", () => {
+//     bringToFront(win);
+//   });
+// });
+
+// function makeDraggable(element) {
+//   let pos1 = 0,
+//     pos2 = 0,
+//     pos3 = 0,
+//     pos4 = 0;
+
+//   const header = element.querySelector(".title-bar");
+
+//   if (header) {
+//     header.onmousedown = dragMouseDown;
+//   }
+
+//   function dragMouseDown(e) {
+//     e = e || window.Event;
+//     e.preventDefault();
+
+//     pos3 = e.clientX;
+//     pos4 = e.clientY;
+
+//     document.onmouseup = closeDragElement;
+//     document.onmousemove = elementDrag;
+//   }
+
+//   function elementDrag(e) {
+//     e = e || window.Event;
+//     e.preventDefault();
+
+//     pos1 = pos3 - e.clientX;
+//     pos2 = pos4 - e.clientY;
+//     pos3 = e.clientX;
+//     pos4 = e.clientY;
+
+//     element.style.transform = "none";
+//     element.style.top = element.offsetTop - pos2 + "px";
+//     element.style.left = element.offsetLeft - pos1 + "px";
+//   }
+
+//   function closeDragElement() {
+//     document.onmouseup = null;
+//     document.onmousemove = null;
+//   }
+// }
+
+// document.querySelectorAll(".window").forEach((win) => {
+//   makeDraggable(win);
+// });
+
 function attClock() {
   const now = new Date();
 
@@ -102,98 +194,6 @@ function attDate() {
     dateElement.textContent = formatedDate;
   }
 }
-
-function openWindow(windowID) {
-  const windowElement = document.getElementById(windowID);
-  if (windowElement) {
-    windowElement.style.display = "flex";
-    bringToFront(windowElement);
-  }
-}
-
-function closeWindow(windowID) {
-  const windowElement = document.getElementById(windowID);
-  if (windowElement) {
-    windowElement.style.display = "none";
-  }
-}
-
-let zIndexCounter = 100;
-
-function bringToFront(element) {
-  zIndexCounter++;
-  element.style.zIndex = zIndexCounter;
-}
-
-document.getElementById("btn-projects").addEventListener("click", (e) => {
-  e.preventDefault();
-  openWindow("window-projects");
-});
-
-document.getElementById("btn-contact").addEventListener("click", (e) => {
-  e.preventDefault();
-  openWindow("window-contact");
-});
-
-document.querySelectorAll(".close-btn").forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const targetID = btn.getAttribute("data-target");
-    closeWindow(targetID);
-  });
-});
-
-document.querySelectorAll(".window").forEach((win) => {
-  win.addEventListener("mousedown", () => {
-    bringToFront(win);
-  });
-});
-
-function makeDraggable(element) {
-  let pos1 = 0,
-    pos2 = 0,
-    pos3 = 0,
-    pos4 = 0;
-
-  const header = element.querySelector(".title-bar");
-
-  if (header) {
-    header.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.Event;
-    e.preventDefault();
-
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-
-    document.onmouseup = closeDragElement;
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.Event;
-    e.preventDefault();
-
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-
-    element.style.transform = "none";
-    element.style.top = element.offsetTop - pos2 + "px";
-    element.style.left = element.offsetLeft - pos1 + "px";
-  }
-
-  function closeDragElement() {
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
-
-document.querySelectorAll(".window").forEach((win) => {
-  makeDraggable(win);
-});
 
 setInterval(attClock, 1000);
 
