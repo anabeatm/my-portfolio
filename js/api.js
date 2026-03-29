395d96b6dd8cd0ee2d7799427d09550eb5baf3f7
Ana Malaquias <ana.beat20051@gmail.com>
2026-03-29 15:52:09 -0300

    feat: add weather API fetching function to retrieve current weather data

--- js/api.js
+++ js/api.js
@@ -14,3 +14,20 @@ export async function getGithubRepos(repos) {
     throw error;
   }
 }
+
+export async function getWeather(latitude, longitude, apiKey) {
+  const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${latitude},${longitude}&lang=pt`;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch Weather API");
+    }
+
+    return await response.json();
+  } catch (error) {
+      console.error("Weather API error: ", error);
+      throw error;
+  }
+}
